e1a7894d285b2234b4ee81c8b7efb6f242a1a8e7
Kiran DBx <mailme.kirandbe@gmail.com>
2025-05-17 02:51:01 +0530

    Add terraform infra files, update .gitignore, add setup notes

--- terraform/aws-postgresql-rds/main.tf
+++ terraform/aws-postgresql-rds/main.tf
@@ -2,19 +2,31 @@ provider "aws" {
   region = var.aws_region
 }
 
+
 resource "aws_db_instance" "postgres" {
   identifier             = var.db_identifier
   allocated_storage      = var.db_allocated_storage
   engine                 = "postgres"
   engine_version         = var.db_engine_version
   instance_class         = var.db_instance_class
-  username               = var.db_username
-  password               = var.db_password
+  #username               = var.db_username
+  #password               = var.db_password
+  username               = local.db_creds.username
+  password               = local.db_creds.password 
   publicly_accessible    = true
   skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [var.security_group_id]
 }
+
+data "aws_secretsmanager_secret_version" "db_secret" {
+  secret_id = "rds/dev-postgres/testdb/testuser"
+}
+
+locals {
+  db_creds = jsondecode(data.aws_secretsmanager_secret_version.db_secret.secret_string)
+}
+
 
 resource "aws_db_subnet_group" "db_subnet" {
   name       = "${var.db_identifier}-subnet-group"
--- terraform/aws-postgresql-rds/variables.tf
+++ terraform/aws-postgresql-rds/variables.tf
@@ -4,8 +4,11 @@ variable "db_allocated_storage" {}
 variable "db_engine_version" {}
 variable "db_instance_class" {}
 variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
+
+# Removed db_username and db_password because we're using Secrets Manager
+#variable "db_username" {}
+#variable "db_password" {}
+
 variable "subnet_ids" {
   type = list(string)
 }
